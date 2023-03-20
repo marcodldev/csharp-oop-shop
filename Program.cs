a45c406e473d8dc83462cd029f4d0501203528c4
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-20 16:10:56 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using csharp_oop_shop;
+
+
+Shop Powerbank = new Shop();
+
+Powerbank.Nome = "Powerbank sfk300ultra";
+Powerbank.Descrizione = "Questo è un prodotto troppo bello e costa pure poco";
+Powerbank.Prezzo = 9.99;
+Powerbank.Iva = 0.22m;
+Console.WriteLine("Codice: " + Powerbank.Codice);
+Console.WriteLine("Nome: " + Powerbank.Nome);
+Console.WriteLine("Descrizione: " + Powerbank.Descrizione);
+Console.WriteLine("Prezzo: " + Powerbank.Prezzo);
+Console.WriteLine("Iva: " + Powerbank.Iva);
---
+++ Shop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_oop_shop
+{
+    public class Shop
+    {
+
+     /*   Un prodotto è caratterizzato da:
+    - codice(numero intero)
+    - nome
+    - descrizione
+     prezzo
+    - iva
+    Usate opportunamente i livelli di accesso(public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+    alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+    Il codice prodotto sia accessibile solo in lettura
+        Gli altri attributi siano accessibili sia in lettura che in scrittura
+    Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+    Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+    Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto. */
+
+        public int Codice { get; private set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public decimal Iva { get; set; }
+
+        public Shop()
+        {
+            Random random = new Random();
+            Codice = random.Next(0, 1000);
+        }
+
+    }
+
+}
